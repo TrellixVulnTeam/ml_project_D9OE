ffc28f14d7edf5cc0fc774c20edd520bba5d5624
Abhishek Nagpal <abhisheknagpal@Abhisheks-MacBook-Pro.local>
2022-07-05 15:21:57 +0530

    updated

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from housing.logger import logging
 app = Flask(__name__)
 
 @app.route('/',methods = ["GET","POST"])
 def index():
+    logging.info("We are testing logging module")
     return "Hello My Name is Abhishek Nagpal.This is my first deployed app."
 
 if __name__ == '__main__':
